cac0727c9365a5d48ada87c59596f78011e9fa58
Alexander Drapun <dr.sasha2602@mail.ru>
2024-07-16 01:47:17 +0300

    Sounds for all buttons

--- Assets/Scripts/MainMenuEvents.cs
+++ Assets/Scripts/MainMenuEvents.cs
@@ -1,46 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 
 public class MainMenuEvents : MonoBehaviour
 {
-
     public static bool SettingsMenu;
     public GameObject openSettingsMenu;
     public int sceneNumber;
     private AudioSource audioSourceBackgroundMusic;
+    private AudioSource onButtonClick;
+
 
     private void Awake()
     {
-        audioSourceBackgroundMusic = GetComponent<AudioSource>();
+        List<AudioSource> audioSources = GetComponents<AudioSource>().ToList();
+        audioSourceBackgroundMusic = audioSources[0];
+        onButtonClick = audioSources[1];
         audioSourceBackgroundMusic.Play();
 
+        Button[] buttons = FindObjectsOfType<Button>();
+
+        // Выводим имена всех кнопок в консоль
+        foreach (Button button in buttons)
+        {
+            button.onClick.AddListener(OnButtonClickPlay);
+        }
     }
+
     public void SettingsButton()
     {
-        audioSourceBackgroundMusic.Play();
+        onButtonClick.Play();
         openSettingsMenu.SetActive(true);
         SettingsMenu = true;
     }
 
     public void BackToMenu()
     {
-        audioSourceBackgroundMusic.Play();
         openSettingsMenu.SetActive(false);
         SettingsMenu = false;
     }
+
+    public void OnButtonClickPlay()
+    {
+        Debug.Log("Button clicked!");
+        onButtonClick.Play();
+    }
+
+
     public void ChangeScene()
     {
         SceneManager.LoadScene(sceneNumber);
     }
     public void StartButton()
     {
-        audioSourceBackgroundMusic.Play();
         ChangeScene();
     }
 }
