c438440b981099d6ae63a4814931a7f15bc35f74
Sanya313 <62214494+Sanya313@users.noreply.github.com>
2024-07-15 23:29:42 +0300

    Added buttons to Education scene
    Added buttons to Education scene

---
+++ Assets/Scripts/EducationButtons.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EducationButtons : MonoBehaviour
+{
+    public float alpha = 0.1f;
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = alpha;
+    }
+}
